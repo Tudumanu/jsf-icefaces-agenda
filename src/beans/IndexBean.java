ea957964ea4f10ccdc49bb56d9db2bcdb7a9c8b8
TadeuBAS <tudumanu@gmail.com>
2016-05-21 14:32:39 -0300

    Inserindo via ajax com icefaces

--- src/beans/IndexBean.java
+++ src/beans/IndexBean.java
@@ -1,19 +1,63 @@
 package beans;
 
+import dao.ContatoDAO;
+import model.Contato;
+
+import javax.faces.bean.CustomScoped;
 import javax.faces.bean.ManagedBean;
+import java.util.List;
 
 @ManagedBean
+@CustomScoped(value = "#{window}")
 public class IndexBean {
-    private String valor;
 
-    public String adicionar() {
-        System.out.println("ADDDDDDDDDDDDD");
+    private String msg;
+    private Contato contato;
+    private ContatoDAO contatoDAO;
+    private List<Contato> listaContato;
 
-        valor = "add";
-        return "adicionar";
+
+    public IndexBean() {
+        System.out.println("IndexBean");
+        init();
     }
 
-    public String getValor() {
-        return valor;
+    private synchronized void init() {
+        System.out.println("init");
+
+        contato = new Contato();
+        contatoDAO = new ContatoDAO();
+
+        listaContato = contatoDAO.search("%");
+    }
+
+    public void insert() {
+        System.out.println("insert");
+
+        msg = contatoDAO.insert(contato) ?
+                contato.getNome() + " inserido com sucesso!"
+                : "Ocorreu um erro ao inserir.";
+    }
+
+    public void update() {
+        System.out.println("update");
+    }
+
+    public void delete() {
+        System.out.println("delete");
+    }
+
+    // GETTERS AND SETTERS //
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public Contato getContato() {
+        return contato;
+    }
+
+    public void setContato(Contato contato) {
+        this.contato = contato;
     }
 }
